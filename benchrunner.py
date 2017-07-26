b524a6cbf703883de735d5953416fc1933bd0ddb
Timo Stüber <timo.stueber@contact-software.com>
2017-07-26 16:21:31 +0200

    log format changed

--- benchrunner.py
+++ benchrunner.py
@@ -11,15 +11,15 @@ loggingConfigFile = 'loggingConf.json'
 
 
 def main():
-    logger.info("[Benchrunner]: Starting")
-    logger.info("[Benchrunner]: Reading the benchsuite: " + suite_file)
+    logger.info("Starting")
+    logger.info("Reading the benchsuite: " + suite_file)
     data = loadJSONData()
     results = {'results': {}}
     # iterating the suite
     for bench_key, bench_val in data["suite"].iteritems():
         if "active" in bench_val and (bench_val["active"] is False):
             continue
-        logger.info("[Benchrunner]: Execute bench: " + bench_key)
+        logger.info("Execute bench: " + bench_key)
         result = start_bench_script(bench_val["file"], bench_val["className"], bench_val["args"])
         results['results'][bench_key] = {'args': bench_val["args"], 'data': result}
     saveJSONData(results)
@@ -33,16 +33,16 @@ def loadJSONData():
         with open(suite_file) as data_file:
             data = json.load(data_file)
     except IOError as err:
-        logger.error("[Benchrunner]: Could not open benchsuite! " + str(err))
+        logger.error("Could not open benchsuite! " + str(err))
         sys.exit(1)
     except ValueError as err:  # JSONDecodeError inherrits from ValueError
-        logger.error("[Benchrunner]: Could not decode benchsuite! " + str(err))
+        logger.error("Could not decode benchsuite! " + str(err))
         sys.exit(1)
     except:
-        logger.error("[Benchrunner]: Unexpected error occurred! " + str(sys.exc_info()[0:1]))
+        logger.error("Unexpected error occurred! " + str(sys.exc_info()[0:1]))
         sys.exit(1)
     else:
-        logger.info("[Benchrunner]: Reading successful")
+        logger.info("Reading successful")
     return data
 
 
@@ -50,20 +50,20 @@ def saveJSONData(data):
     """
     This functions dumps json data into a file.
     """
-    logger.info("[Benchrunner]: Saving Results to file: " + output_file)
+    logger.info("Saving Results to file: " + output_file)
     try:
         with open(output_file, 'w') as outfile:
             json.dump(data, outfile, sort_keys=True, indent=4)
     except IOError as err:
-        logger.error("[Benchrunner]: Could not open file to save the data! " + str(err))
+        logger.error("Could not open file to save the data! " + str(err))
     except ValueError as err:  # JSONDecodeError inherrits from ValueError
-        logger.error("[Benchrunner]: Could not decode values! " + str(err))
+        logger.error("Could not decode values! " + str(err))
     except TypeError as err:
-        logger.error("[Benchrunner]: Could not serialize object! " + str(err))
+        logger.error("Could not serialize object! " + str(err))
     except:
-        logger.error("[Benchrunner]: Unexpected error occurred! " + str(sys.exc_info()[0:1]))
+        logger.error("Unexpected error occurred! " + str(sys.exc_info()[0:1]))
     else:
-        logger.info("[Benchrunner]: Saving successful")
+        logger.info("Saving successful")
 
 
 def start_bench_script(path, className, args):
@@ -74,13 +74,13 @@ def start_bench_script(path, className, args):
         mod = __import__(path, fromlist=[className])
         bench_class = getattr(mod, className)
     except ImportError as err:
-        logger.error("[Benchrunner]: Could not import bench file! " + str(err))
+        logger.error("Could not import bench file! " + str(err))
         return
     except AttributeError as err:
-        logger.error("[Benchrunner]: Could not find className: {0}! {1}".format(className, str(err)))
+        logger.error("Could not find className: {0}! {1}".format(className, str(err)))
         return
     except:
-        logger.error("[Benchrunner]: Unexpected error occurred! " + str(sys.exc_info()[0:1]))
+        logger.error("Unexpected error occurred! " + str(sys.exc_info()[0:1]))
         return
 
     # perform the benchmark
@@ -88,9 +88,15 @@ def start_bench_script(path, className, args):
 
 
 if __name__ == "__main__":
+    # removing the root handlers
+    for handler in logging.root.handlers[:]:
+        logging.root.removeHandler(handler)
     # initialize the logging
-    logging.basicConfig()
+    logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s] %(name)s: %(message)s")
+
+    # try to read config
     with open(loggingConfigFile, "r") as configFile:
         logging.config.dictConfig(json.load(configFile))
-    logger = logging.getLogger(__name__)
+        pass
+    logger = logging.getLogger("[Benchrunner]")
     main()
