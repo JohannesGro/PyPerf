1679f8d6e4fa05afb9a7ae28e41b58e5d27293f2
Timo Stüber <timo.stueber@contact-software.com>
2017-07-31 14:47:35 +0200

    changed documentation + sorted method calls

--- bench/bench.py
+++ bench/bench.py
@@ -9,17 +9,18 @@ class Bench(object):
     results = []
 
     def setUp(self):
-        """Method called to prepare the test fixture. The default implementation does nothing."""
+        """Method called to prepare the test fixture. The default implementation does nothing.
+        """
         pass
 
     def tearDown(self):
         """Method called immediately after the test method has been called and
-        the result recorded.  The default implementation does nothing. """
+        the result recorded.  The default implementation does nothing.
+        """
         pass
 
     def setUpClass(self):
-        """
-        This method is called before the first test and is used for preparing all test.
+        """This method is called before the first test and is used for preparing all test.
         """
         pass
 
@@ -30,6 +31,11 @@ class Bench(object):
         pass
 
     def storeResult(self, res):
+        """Store the benchmark result. The run-Method call will be return all stored results.
+
+        Keyword arguments:
+        res -- a dictionary with a format spezified in the field res['type']
+        """
         curframe = inspect.currentframe()
         calframe = inspect.getouterframes(curframe, 2)
         self.results.append({calframe[1][3]: res})
@@ -38,21 +44,20 @@ class Bench(object):
         """
         This method calls every test in this class. Test are indentified by the prefix 'test_'.
         The setUp-method is called immediately before each test and the tearDown-method is
-        called immediately after each test. The result is structured in json format and will be returned.
+        called immediately after each test. The result of benchmark is structured in json format and will be returned.
+
+        Keyword arguments:
+        args -- a dictionary consisting of all parameter which are used in this benchmark. E.g. iterations could be used for the repitition of an insert query.
         """
         self.args = args
         self.results = []
         try:
             self.setUpClass()
-            for test, val in self.__class__.__dict__.iteritems():
+            for test, val in sorted(self.__class__.__dict__.iteritems()):
                 if test.find('bench_') == 0:
                     test_method = getattr(self, test)
                     self.setUp()
-                    # each test need to return [val, unit]
-                    result = test_method()
-                    if result:
-                        # results.append({test_method.__name__: {'value': result[0], 'unit': result[1]}})
-                        self.results.append({test_method.__name__: result})
+                    test_method()
                     self.tearDown()
             self.tearDownClass()
         except:
