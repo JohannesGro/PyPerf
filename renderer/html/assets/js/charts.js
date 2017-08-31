20a160f06c43971dc608777a400a28bc74ffebcd
Timo Stüber <timo.stueber@contact-software.com>
2017-08-31 10:20:43 +0200

    extend chart for multiple files

--- renderer/html/assets/js/charts.js
+++ renderer/html/assets/js/charts.js
@@ -1,12 +1,31 @@
 function createBarChart(DOMElement, data) {
 
+  var files = [];
+  for(var i = 0 ; i < data.length; i++) {
+    var d = data[i];
+    if( -1 ==  files.indexOf(d.file)) {
+      files.push(d.file);
+    }
+  }
+  var num_files = files.length;
+
+  data.sort(function(a, b) {
+      return a.name.localeCompare(b.name);
+  });
+
+  // Color scale
+  var color = d3.scaleOrdinal(d3.schemeCategory20);
+  var gapBetweenGroups = 5;
+  var numGroups = data.length / num_files;
+
   var margin = {top: 20, right: 30, bottom: 40, left: 200},
       width = 960 - margin.left - margin.right,
-      height = (500 /8*data.length) - margin.top - margin.bottom;
+      height = (500 / 15) * data.length + gapBetweenGroups * numGroups * (num_files - 1);
+
 
   var x = d3.scaleLinear().rangeRound([0, width]);
 
-  var y = d3.scaleBand().rangeRound([height, 0]).paddingInner(0.15);;
+  var y = d3.scaleBand().rangeRound([height, 0]).paddingInner(0.1);;
 
   var xAxis = d3.axisBottom()
       .scale(x)
@@ -16,7 +35,8 @@ function createBarChart(DOMElement, data) {
       .tickSize(0)
       .tickPadding(6);
 
-  var barHeight = 40;
+
+
   var svg = d3.select(DOMElement).append("svg")
       .attr("width", width + margin.left + margin.right)
       .attr("height", height + margin.top + margin.bottom)
@@ -26,6 +46,8 @@ function createBarChart(DOMElement, data) {
 
     x.domain(d3.extent(data, function(d) { return d.value; })).nice();
     y.domain(data.map(function(d) { return d.name; }));
+  var barHeight = (y.bandwidth() - (gapBetweenGroups * (num_files - 1))) / num_files;
+  console.log((y.bandwidth() - (gapBetweenGroups * (num_files - 1))) / num_files);
 
     var bar = svg.selectAll(".bar")
           .data(data)
@@ -34,15 +56,16 @@ function createBarChart(DOMElement, data) {
 
     bar.append("rect")
         .attr("x", 0)
-        .attr("y", function(d) { return y(d.name) })
+        .attr("y", function(d, i) { return y(d.name) + ((i%num_files)  * (barHeight + gapBetweenGroups))})
         .attr("width", function(d) { return x(d.value) - x(0); })
-        .attr("height", y.bandwidth());
+        .attr("height", barHeight)
+        .attr("fill", function(d,i) { return color(i % num_files); });
 
 
     var formatVal = d3.format(".5f");
     bar.append("text")
         .attr("x", function(d) { return x(d.value) + 1 ; })
-        .attr("y", function(d) { return y(d.name) + y.bandwidth()/2;})
+        .attr("y", function(d, i) { return y(d.name) + ((i%num_files)  * (barHeight + gapBetweenGroups)) + barHeight / 2 ;})
         .attr("dy", ".35em")
       //  .attr("text-anchor", "middle")
         .attr("alignment-baseline", "baseline")
@@ -58,14 +81,36 @@ function createBarChart(DOMElement, data) {
         .attr("class", "y axis")
         .call(yAxis);
 
-/*
 
-    bar.append("text")
-        .attr("dy", ".75em")
-        .attr("y", function(d) { return y(d.name); } )
-        .attr("x", function(d) { return x(d.value); }  )
-        .text(function(d) { return formatVal(d.value); });
-*/
+    var legendRectSize = 18,
+        legendSpacing  = 4,
+        spaceForLabels = 10;
+
+
+    var legend = svg.selectAll('.legend')
+        .data(files)
+        .enter()
+        .append('g')
+        .attr('transform', function (d, i) {
+            var height = legendRectSize + legendSpacing;
+            var offset = -gapBetweenGroups/2;
+            var horz = spaceForLabels + width + 40 - legendRectSize;
+            var vert = i * height - offset;
+            return 'translate(' + horz + ',' + vert + ')';
+        });
+
+    legend.append('rect')
+        .attr('width', legendRectSize)
+        .attr('height', legendRectSize)
+        .style('fill', function (d, i) { return color(i); })
+        .style('stroke', function (d, i) { return color(i); });
+
+
+    legend.append('text')
+        .attr('class', 'legend')
+        .attr('x', legendRectSize + legendSpacing)
+        .attr('y', legendRectSize - legendSpacing)
+        .text(function (d) { return d; });
 
 
 }
