7d4ffb91498db43d6485b3ab091069717d8ecc39
Timo Stüber <timo.stueber@contact-software.com>
2017-09-08 13:41:44 +0200

    changed to cdb import + longsdesc

--- setup.py
+++ setup.py
@@ -1,10 +1,19 @@
 ﻿# coding: utf-8
 
-from setuptools import setup
+from cdb.comparch.pkgtools import setup
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(name='benchmarktool',
       version='0.1',
-      description='Create and render benchmarks',
+      description='This project aims to provide a base for creating and rendering standardized benchmarks.',
+      long_description=long_description,
       url='http://git.contact.de/tst/Benchmarking-Tool',
       author='Timo Stüber',
       author_email='Timo.Stueber@contact-software.com',
