d1b72b61e78fc5a675fa081d0ef782e43e7107a6
Timo Stüber <timo.stueber@contact-software.com>
2017-09-13 10:06:10 +0200

    stashed apply

---
+++ README.rst
@@ -0,0 +1,9 @@
+Benchmarktool
+=======================
+
+The benchmarktool project aims to provide a base for writing standardized benchmarks.
+For this purpose the project has two main parts. Firstly a runner executes the
+benchmakrs and saves the result. Secondly a renderer reads the results and displays
+it in a human readable manner.
+It is possible to define a suite of benchmarks, run the suite on several systems
+and compare the results.
---
+++ benchmarktool/bench_test.py
@@ -0,0 +1,95 @@
+#!c:\ce\trunk\win32\debug\img\python.exe
+# -*- mode: python; coding: utf-8 -*-
+#
+# Copyright (C) 1990 - 2017 CONTACT Software GmbH
+# All rights reserved.
+# https://www.contact-software.com/
+
+
+import unittest
+
+from bench.bench import Bench
+
+
+class test_class(Bench):
+
+    testString = []
+
+    def setUp(self):
+        self.testString.append("setUp")
+
+    def tearDown(self):
+        self.testString.append("tearDown")
+
+    def setUpClass(self):
+        self.testString.append("setUpClass")
+
+    def tearDownClass(self):
+        self.testString.append("tearDownClass")
+
+    def bench_A(self):
+        self.testString.append("bench_A")
+
+    def bench_B(self):
+        self.testString.append("bench_B")
+        self.help()
+
+    def bench_C(self):
+        self.testString.append("bench_C")
+
+    def help(self):
+        self.testString.append("help")
+
+
+class TestBenchMethods(unittest.TestCase):
+
+    def setUp(self):
+        self.t = test_class()
+
+    def test_exec_order(self):
+        expected = ["setUpClass", "setUp", "bench_A", "tearDown", "setUp", "bench_B", "help", "tearDown", "setUp", "bench_C", "tearDown", "tearDownClass"]
+        self.t.run({})
+        self.assertEqual(len(self.t.testString), len(expected))
+        self.assertEqual(self.t.testString, expected)
+
+    def test_storeResult1(self):
+        # {self.namespace + name: {"value": val, "unit": unit, "type": type}}
+        val = [1, 2, 3]
+        expected = {"test_storeResult1": {"value": val, "unit": "hour", "type": "time_series"}}
+        self.t.storeResult(val, name="", type="time_series", unit="hour")
+        self.assertTrue(expected.viewitems() <= self.t.results.viewitems())
+
+    def test_storeResult2(self):
+        # {self.namespace + name: {"value": val, "unit": unit, "type": type}}
+        val = [1, 2, 3]
+        expected = {"test_storeResult2": {"value": val, "unit": "seconds", "type": "time_series"}}
+        self.t.storeResult(val, name="", type="time_series", unit="seconds")
+        self.assertTrue(expected.viewitems() <= self.t.results.viewitems())
+
+    def test_storeResult3(self):
+        # {self.namespace + name: {"value": val, "unit": unit, "type": type}}
+        val = 1
+        expected = {"test_storeResult3": {"value": val, "unit": "seconds", "type": "time"}}
+        self.t.storeResult(val)
+        self.assertTrue(expected.viewitems() <= self.t.results.viewitems())
+
+    def test_storeResult4(self):
+        # {self.namespace + name: {"value": val, "unit": unit, "type": type}}
+        self.t.namespace = "prefix_"
+        val = [1, 2, 3]
+        expected = {"prefix_test_storeResult4": {"value": val, "unit": "seconds", "type": "time"}}
+        self.t.storeResult(val)
+        self.assertTrue(expected.viewitems() <= self.t.results.viewitems())
+
+    def test_discard(self):
+        # {self.namespace + name: {"value": val, "unit": unit, "type": type}}
+        val = [1, 2, 3]
+        expected = {"test_discard": {"value": val, "unit": "seconds", "type": "time_series"}}
+        self.t.storeResult(val, name="", type="time_series", unit="seconds")
+        self.assertTrue(expected.viewitems() <= self.t.results.viewitems())
+        self.t.discard("test_")
+        self.assertFalse(expected.viewitems() <= self.t.results.viewitems())
+
+
+if __name__ == '__main__':
+    unittest.main()
