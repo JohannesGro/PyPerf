78cc6f21bc9c6f3fe8eb9e5876e94aee04669334
Waleri Enns <wen@contact.de>
2018-03-12 14:43:39 +0100

    coverage tooling the #2

---
+++ benchmarktool/tests/test_benchrunner.py
@@ -0,0 +1,41 @@
+# -*- mode: python; coding: utf-8 -*-
+#
+# Copyright (C) 1990 - 2018 CONTACT Software GmbH
+# All rights reserved.
+# https://www.contact-software.com/
+
+import unittest
+import os
+
+from ..benchrunner import Benchrunner
+from nose.tools import eq_
+
+class TestBenchrunner(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_normalize_bench_path(self):
+        # positive cases:
+        # - both absolute
+        # - benchpath is relative, the suite is absolute
+        # - benchpath is relative, the suite is also relative
+
+        cwd = os.getcwd()
+        print cwd
+        cases = [
+            # <suitepath>, <benchpath>, <expected result>
+            ("suite.json", "/benchmark.py", "/benchmark.py"),
+            ("/suite.json", "/benchmark.py", "/benchmark.py"),
+            ("/suite.json", "benchmark.py", "/benchmark.py"),
+            ("/folder/suite.json", "benchmark.py", "/folder/benchmark.py"),
+            ("/folder/suite.json", "subfolder/benchmark.py", "/folder/subfolder/benchmark.py"),
+            ("folder/suite.json", "subfolder/benchmark.py", os.path.join(cwd, "folder/subfolder/benchmark.py")),
+
+            # one backstep case
+            ("/folder/../suite.json", "benchmark.py", "/benchmark.py")
+        ]
+
+        benchrunner = Benchrunner()
+        for suitepath, benchpath, expected_result in cases:
+            result = benchrunner.normalize_bench_path(suitepath, benchpath)
+            eq_(result, expected_result)
---
+++ tests/utils.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- mode: python; coding: utf-8 -*-
+#
+# Copyright (C) 1990 - 2018 CONTACT Software GmbH
+# All rights reserved.
+# https://www.contact-software.com/
+
+import os
+
+def coverage_opts():
+    opts = []
+    opts_env = os.environ.get("COVERAGE_OPTS", None)
+    if opts_env:
+        opts = opts_env.split(" ")
+    return opts
