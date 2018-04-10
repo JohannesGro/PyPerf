2e38eb83a9faa5644dad99a2280983496eb7f941
Waleri Enns <wen@contact.de>
2018-04-10 10:24:42 +0200

    Make callable as a module (python -m pyperf)

---
+++ pyperf/__main__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- python -*- coding: iso-8859-1 -*-
+#
+# Copyright (C) 1990 - 2018 CONTACT Software GmbH
+# All rights reserved.
+# http://www.contact.de/
+#
+
+import sys
+from benchmark import main
+if __name__ == "__main__":
+    sys.exit(main())
